e8cf24f3775b835c09866269ee5a40251a5bd2ae
kaans <kaans@DESKTOP-U4CBDT5>
2016-10-06 11:14:40 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace _12_Employee
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public long Id { get; set; }
+
+        public Employee(long id, string name, string type)
+        {
+            Name = name;
+            Type = type;
+            Id = id;
+        }
+
+        
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+        public List<Employee> employeesList;
+        private long id;
+        public EmployeeRepository()
+        {
+            employeesList = new List<Employee>();
+        }
+        internal void Clear()
+        {
+            employeesList.Clear();
+        }
+
+        internal int CountEmployees()
+        {
+            return employeesList.Count;
+        }
+        public long NextId()
+        {
+            id++;
+            return id;
+        }
+        internal Employee CreateEmployee(string name, string profession)
+        {
+            NextId();
+            Employee emp = new Employee(id, name, profession);
+            employeesList.Add(emp);
+            return emp;
+
+        }
+
+        internal void SaveEmployee(Employee simon)
+        {
+            NextId();
+            Employee emp = new Employee(simon.Id, simon.Name, simon.Type);
+        }
+
+        internal Employee LoadEmployee(long id)
+        {
+            foreach(Employee emp in employeesList)
+            {
+                if (emp.Id == id)
+                    return emp;
+            }
+            return null;
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            return employeesList;
+        }
+    }
+}
